d24b7454481aa458093732d2007e7b52d9d11b4d
Bleoo <etwarrob@my.erau.edu>
2024-04-11 21:52:51 -0400

    - commented out some code - changed line 10 to reflect renaming of charCreate

--- main.m
+++ main.m
@@ -1,28 +1,26 @@
-%main file
+%main
 
 %delete previous data
-clear;
-clc;
-close all;
+clear;clc;close all;
 
-%density of population
-n = 1000;
+%population density
+n = 100;
 
 %assign physical characteristic parameters
-person = physChar(n);
+character = charCreate(n);
 
-%apply preference equation to each agent
-precNum = precEq(person,n);
-
-%using the interp1 to map the perceived attractiveness values to 1-10
-%might be able to put this block of code in the precNum function
-origVal = [min(precNum),max(precNum)];
-newVal = [1,10];
-vq1 = round(interp1(origVal,newVal,precNum))'; %rounded values for discrete numbers
-
-%data
-mData = mean(vq1); %mean of the data
-sDev = std (vq1); %standard deviation of the data (is this supposed to be noise?)
-
-%graph the values
-histfit(vq1) %makes a histogram and applies a density function to it
+% %apply preference equation to each agent
+% precNum = precEq(person,n);
+% 
+% %using the interp1 to map the perceived attractiveness values to 1-10
+% %might be able to put this block of code in the precNum function
+% origVal = [min(precNum),max(precNum)];
+% newVal = [1,10];
+% vq1 = round(interp1(origVal,newVal,precNum))'; %rounded values for discrete numbers
+% 
+% %data
+% mData = mean(vq1); %mean of the data
+% sDev = std (vq1); %standard deviation of the data (is this supposed to be noise?)
+% 
+% %graph the values
+% histfit(vq1) %makes a histogram and applies a density function to itplot(X,Y);
